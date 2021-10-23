ede6e775cb5afafdccc0fe0b189ac3ee6ce395ff
Eric Barthel <ebarthel82@gmail.com>
2021-10-23 02:14:03 -0500

    Finished event methods

--- src/edu/century/project1/Event.java
+++ src/edu/century/project1/Event.java
@@ -1,5 +1,7 @@
 package edu.century.project1;
 
+import java.util.Random;
+
 public class Event {
 	private String name;
 	private Venue venue;
@@ -7,43 +9,82 @@ public class Event {
 	private Athlete[] athletes;
 	
 	public Event() {
+		name = "DEFAULT";
+		venue = Venue.selectRandomVenue();
+		sport = Sport.selectRandomSport();
+		athletes = new Athlete[2];
+		athletes[1] = new Athlete();
+		athletes[2] = new Athlete();
 		
 	}
-	public Event(String, Venue, Sport, Athlete) {
-		
+	public Event(String userName, Venue userVenue, Sport userSport, Athlete[] userInput) {
+		name = userName;
+		venue = userVenue;
+		sport = userSport;
+		athletes = new Athlete[userInput.length];
+		for (int i = 0; i < userInput.length; i++) {
+			athletes[i] = userInput[i];
+		}
 	}
 	public String getName()  {
-		
+		return name;
 	}
-	public void setName(String) {
-		
+	public void setName(String input) {
+		name = input;
 	}  
 	public Venue getVenue()  {
-		
+		return venue;
 	}
-	public Venue setVenue() {
-		
+	public void setVenue(Venue input) {
+		venue = input;
 	}
 	public Sport getSport() {
-		
+		return sport;
 	}
-	public void setSport(Sport) {
-		
+	public void setSport(Sport input) {
+		sport = input;
 	}
 	public Athlete[] getAthletes() {
-		
+		return athletes;
 	}
-	public void setAthlete(Athlete[]) {
-		
+	public void setAthlete(Athlete[] userInput) {
+		for (int i = 0; i < userInput.length; i++) {
+			athletes[i] = userInput[i];
+		}
 	}
 	public String toString() {
+		return "Event Name: " + name + "/nVenue: " + venue + "/nSport: " + sport + "/nAthletes: " + athletes;
+	}
+	public boolean equals(Object otherObj) {
+		if (otherObj == null) {
+			return false;
+			}
+		else if (getClass() != otherObj.getClass()) {
+			return false;
+		}
+		else {
+			Event otherEvent = (Event)otherObj;
+			return (this.name.equals(otherEvent.getName())) && (this.venue.equals(otherEvent.getVenue())) && 
+					(this.sport.equals(otherEvent.getSport())) && (this.athletes.equals(otherEvent.getAthletes())) ; 
+		}
+	}
+	
+	public Athlete compete(Athlete[] userInput) {
+		for (int i = 0; i < userInput.length; i++) {
+			Random rand = new Random();
+			int performance = rand.nextInt(50) + 1;
+			int temp = athletes[i].getSkill();
+			athletes[i].setSkill(temp + performance); 
+		}
+		
+		Athlete winner = athletes[0];
+		for (int i=0; i < userInput.length; i++) {
+			if (athletes[i].getSkill() > winner.getSkill() ) {
+				winner = athletes[i];
+			}
+		}
+		return winner;
 		
 	}
-	public boolean equals(Object) {
-		
-	}
-	public Athlete compete() {
-		
-	}
-
+	
 }
